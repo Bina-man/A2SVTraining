3e94e9147599b38281854be10c02fd685785b2b5
Binyam Sisay <48756392+Bina-man@users.noreply.github.com>
2026-02-16 06:12:43 +0300

    add: one is sum of others

---
+++ ProgressTracker/one_is_sum_of_others.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    t = int(input())
+    
+    for _ in range(t):
+        a, b, c = map(int, input().split())
+
+        if a == b+c:
+            print("YES")
+        elif b == a+c:
+            print("YES")
+        elif c == a+b:
+            print("YES")
+        else:
+            print("NO")
